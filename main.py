b5eedd09b44d2f8eccedf997b749457e1da129b0
gerardSch <118450930+gerardSch@users.noreply.github.com>
2023-02-25 21:53:22 +0100

    2nd run

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! \n This is my first try'
+    return 'Hello World! \n This is my first try \n this is 2nd run'
 
 @app.route('/echo/<name>')
 def echo(name):
